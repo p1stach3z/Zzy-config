c13c975609f457129ed7bec810d2a7b62c0fae70
Chame <jsamuel.murrieta@gmail.com>
2026-03-23 22:19:51 -0600

    update: 2026-03-23-2219

--- home/users/fv/default.nix
+++ home/users/fv/default.nix
@@ -2,7 +2,7 @@
 { config, pkgs, lib, specialArgs, ... }:
 {
   imports = [
-    inputs.nix-index-database.homeModules.nix-index
+    nix-index-database.homeModules.nix-index
     ./core
     ./programs
     ./desktop
