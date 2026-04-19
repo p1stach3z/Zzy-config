80d4380ffb84d2d87f17a8fb800dc24ec27573c5
Chame <jsamuel.murrieta@gmail.com>
2026-04-19 08:30:20 -0600

    update: 2026-04-19-0830

--- hosts/Zzy/programs/steam-gaming.nix
+++ hosts/Zzy/programs/steam-gaming.nix
@@ -8,15 +8,11 @@
     gamescopeSession = {
       enable = true;
       args = [ "-f" "-F" "fsr" "--fsr-sharpness" "2" "--xwayland-count" "2" "--rt" "--steam" ];
-      steamArgs = [ "-tenfoot" "-pipewire-dmabuf" "-dev" ];
+      steamArgs = [ "-tenfoot" "-pipewire-dmabuf" "-vulkan" "-dev" ];
       env = {
         SDL_VIDEODRIVER = "wayland";
         AMD_VULKAN_ICD = "RADV";
-        WLR_RENDERER = "vulkan";
-        STEAM_GAMESCOPE_FANCY_SCALING_SUPPORT = "1";
         STEAM_ENABLE_VOLUME_HANDLER = "1";
-        STEAM_GAMESCOPE_COLOR_MANAGED = "1";
-        STEAM_GAMESCOPE_VIRTUAL_WHITE = "1";
       };
     };
   };
